c21770c3c4f9fcc2e4d8e38d40a555c3204f381d
kasimovahokume <kasimovahokum@gmail.com>
2026-04-22 02:36:05 +0400

    structur,input created

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import '../styles/reset.css'
+import '../styles/global.css'
+import SocialContainer from '../features/social/components/SocialContainer'
+
+function App() {
+
+
+  return (
+    <>
+    <SocialContainer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import Input from "../Input"
+
+
+const Navbar = (() => {
+  return (
+    <div>Navbar
+      <nav>
+        <div>
+          sarkhanrahimli
+        </div>
+        <Input/>
+      </nav>
+    </div>
+  )
+})
+
+export default Navbar
