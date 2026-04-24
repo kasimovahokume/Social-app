d8fdd220e70bc306346a31200a104ded96da8eaa
Hokume <kasimovahokum@gmail.com>
2026-04-25 00:15:12 +0400

    postModal

--- src/shared/components/Navbar/Navbar.jsx
+++ src/shared/components/Navbar/Navbar.jsx
@@ -1,29 +1,54 @@
-import Input from "../Input"
-import styles from "./Navbar.module.css"
+import Input from "../Input";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <header className={styles.navbar}>
       <div className={styles.navbarContainer}>
+
+        {/* LOGO */}
         <div className={styles.navbarLogo}>
-          sarkhanrahimlidev
+          <span className={styles.logoAccent}>s</span>arkhanrahimlidev
         </div>
 
+        {/* SEARCH */}
         <div className={styles.navbarSearch}>
-          <Input/>
+          <Input />
         </div>
 
+        {/* ACTIONS */}
         <div className={styles.navbarActions}>
-          <div className={styles.iconWrapper}>
+
+          {/* Mail */}
+          <button className={styles.actionBtn} aria-label="Messages">
+            <svg viewBox="0 0 24 24" fill="currentColor" width="22" height="22">
+              <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z" />
+            </svg>
             <span className={styles.badge}>1</span>
-          </div>
-          <div className={styles.iconWrapper}>
+          </button>
+
+          {/* Bell */}
+          <button className={styles.actionBtn} aria-label="Notifications">
+            <svg viewBox="0 0 24 24" fill="currentColor" width="22" height="22">
+              <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" />
+            </svg>
             <span className={styles.badge}>7</span>
+          </button>
+
+          {/* Avatar */}
+          <div className={styles.avatarWrapper}>
+            <img
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="profile"
+              className={styles.avatar}
+            />
+            <span className={styles.onlineDot} />
           </div>
+
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/shared/components/Sidebar/Sidebar.jsx
+++ src/shared/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,87 @@
-import styles from "./Sidebar.module.css"
+import styles from "./Sidebar.module.css";
+
+const menuItems = [
+  {
+    label: "Homepage",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Pages",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Groups",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Marketplace",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M19 6h-2c0-2.76-2.24-5-5-5S7 3.24 7 6H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7-3c1.66 0 3 1.34 3 3H9c0-1.66 1.34-3 3-3zm0 10c-1.66 0-3-1.34-3-3h2c0 .55.45 1 1 1s1-.45 1-1h2c0 1.66-1.34 3-3 3z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Friends",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Settings",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z" />
+      </svg>
+    ),
+  },
+  {
+    label: "Profile",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+      </svg>
+    ),
+  },
+];
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-        <ul className={styles.menuList}>
-            <li className={styles.menuItem}>Homepeage</li>
-            <li className={styles.menuItem}>Pages</li>
-            <li className={styles.menuItem}>Groups</li>
-            <li className={styles.menuItem}>Marketplace</li>
-            <li className={styles.menuItem}>Friends</li>
-            <li className={styles.menuItem}>Settings</li>
-            <li className={styles.menuItem}>Profile</li>
-        </ul>
-    </aside>
-  )
-}
+      <ul className={styles.menuList}>
+        {menuItems.map((item) => (
+          <li key={item.label} className={styles.menuItem}>
+            <span className={styles.menuIcon}>{item.icon}</span>
+            <span className={styles.menuLabel}>{item.label}</span>
+          </li>
+        ))}
+      </ul>
 
-export default Sidebar
+      <div className={styles.themeToggle}>
+        <svg viewBox="0 0 24 24" fill="currentColor" width="18" height="18">
+          <path d="M20 15.31L23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6z" />
+        </svg>
+        <label className={styles.toggle}>
+          <input type="checkbox" />
+          <span className={styles.slider}></span>
+        </label>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
